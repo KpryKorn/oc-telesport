0ae7c821ffd6fa582305f3d1a44e8a9d9724486c
Sacha <sroffini@mgen.fr>
2025-09-08 15:32:00 +0200

    :sparkles: feat: finalize dashboard page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { PieChartConfig } from 'src/app/core/models/PieChart';
 
 @Component({
   selector: 'app-home',
@@ -11,20 +12,18 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[] | null> = of(null);
 
+  public chartConfig: PieChartConfig = {
+    title: 'Médailles par pays - Jeux Olympiques',
+    showLegend: true,
+    legendPosition: 'bottom',
+    showPercentage: false,
+    height: 400,
+  };
+
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympicService.loadInitialData().subscribe();
     this.olympics$ = this.olympicService.getOlympics();
-  }
-
-  getTotalMedalsByCountry(olympics: Olympic[], country: string) {
-    return olympics
-      .filter((o) => o.country === country)
-      .reduce((acc, curr) => {
-        return (
-          acc + curr.participations.reduce((sum, p) => sum + p.medalsCount, 0)
-        );
-      }, 0);
   }
 }
