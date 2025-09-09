64b3197ac54b0fff6b2933026c03862fbb2ec64f
Sacha <sroffini@mgen.fr>
2025-09-09 11:15:40 +0200

    :sparkles: feat(ui): update dashboard page UI

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,6 +12,9 @@ import { PieChartConfig } from 'src/app/core/models/PieChart';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[] | null> = of(null);
 
+  public totalCountries: number = 0;
+  public totalEditions: number = 0;
+
   public chartConfig: PieChartConfig = {
     title: 'Médailles par pays - Jeux Olympiques',
     showLegend: true,
@@ -25,5 +28,32 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.olympicService.loadInitialData().subscribe();
     this.olympics$ = this.olympicService.getOlympics();
+
+    this.olympics$.subscribe((olympics) => {
+      if (olympics && olympics.length > 0) {
+        const uniqueCountries = new Set(olympics.map((o) => o.country));
+        this.totalCountries = uniqueCountries.size;
+
+        const uniqueYears = new Set();
+        olympics.forEach((o) => {
+          o.participations.forEach((p) => {
+            uniqueYears.add(p.year);
+          });
+
+          this.totalEditions = uniqueYears.size;
+        });
+      } else {
+        this.totalCountries = 0;
+        this.totalEditions = 0;
+      }
+    });
+  }
+
+  getTotalNumberOfCountries(): number {
+    return this.totalCountries;
+  }
+
+  getTotalNumberOfEditions(): number {
+    return this.totalEditions;
   }
 }
